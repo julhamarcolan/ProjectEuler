b54424706bfaab07ec3c775ca42dc329bf4227ba
julha Marcolan <47335560+julhamarcolan@users.noreply.github.com>
2020-11-22 19:20:43 -0300

    Update 1.py
    correção ortográfica

--- 1/python/1.py
+++ 1/python/1.py
@@ -1,7 +1,7 @@
 maximo = 1000
 soma = 0 
 
-#Lembra que em python vau até um anterior o máximo range
+#Lembra que em python vai até um anterior o máximo range
 for i in range(1, maximo):
     if (i%3 == 0 ) or (i%5==0):
         soma = soma + i 
